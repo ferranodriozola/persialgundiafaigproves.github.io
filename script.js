2e5a69b8288f6462da86bb4da234fe991c3a03f0
ferranodriozola <155725797+ferranodriozola@users.noreply.github.com>
2024-01-07 18:02:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('.cerca-button').addEventListener('click', realitzarCerca);
-});
 
-function realitzarCerca() {
-    var seleccioCerca = document.querySelector('.seleccio-cerca').value;
-    var inputNumero = document.querySelector('.numero');
-    var resultatText = document.getElementById('resultat-text');
-
-    var multiplicador = (seleccioCerca === 'opcio1') ? 5 : 10;
-    var resultat = +inputNumero.value * multiplicador;
-
-    resultatText.textContent = `El resultat de ${inputNumero.value} multiplicat per ${multiplicador} és: ${resultat}`;
-}
