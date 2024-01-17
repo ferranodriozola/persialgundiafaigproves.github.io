42e2cdbef956e0db2ab72df70ec6fc22bddf9dc0
ferranodriozola <155725797+ferranodriozola@users.noreply.github.com>
2024-01-17 19:37:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,117 +1,94 @@
-// Per les caselles clicables de sota
-function toggleList(listId) {
-    var list = document.getElementById(listId);
-    list.style.display = (list.style.display === 'none') ? 'block' : 'none';
-
-    var checkboxes = list.querySelectorAll('.clickable-checkbox');
+//Botó
+document.addEventListener('DOMContentLoaded', function () {
+    var checkboxes = document.querySelectorAll('.clickable-checkbox');
     checkboxes.forEach(function (checkbox) {
-        checkbox.checked = list.style.display === 'block';
-    });
-}
-
-// JavaScript function to toggle the checkbox
-function toggleButton() {
-    var checkbox = document.getElementById('toggleButton');
-    if (checkbox.checked) {
-        // Checkbox is checked
-        console.log('Checkbox is checked');
-    } else {
-        // Checkbox is unchecked
-        console.log('Checkbox is unchecked');
-    }
-}
-
-
-
-
-
-
-
-// Funció de cerca
-function search() {
-    console.log('Inici de la funció search');
-    const searchInput = document.getElementById('searchInput');
-    const resultDiv = document.getElementById('resultContainer');
-
-    // Obtenir la lletra introduïda
-    const searchTerm = searchInput.value.toLowerCase();
-
-    // Carregar dinàmicament les dades des de l'arxiu de text
-    return fetch('data.txt')
-        .then(response => response.text())
-        .then(data => {
-            console.log('Dades carregades amb èxit');
-            processData(data, searchTerm, resultDiv);
-        })
-        .catch(error => {
-            console.error('Error carregant les dades:', error);
-            resultDiv.innerHTML = 'Error en carregar les dades.';
-        })
-        .finally(() => {
-            console.log('Final de la funció search');
-        });
-}
-
-
-
-
-
-
-
-// Processar les dades i mostrar el resultat
-function processData(data, searchTerm, resultDiv) {
-    console.log('Inici de la funció processData');
-    console.log('Dades rebudes',);
-
-    const dropdowns = document.querySelectorAll('select');
-    const selectedValues = {};
-
-    dropdowns.forEach((dropdown, index) => {
-        const selectedOption = dropdown.options[dropdown.selectedIndex];
-        if (selectedOption) {
-            selectedValues[`dropdown${index + 1}`] = selectedOption.value.toLowerCase();
-        }
+        checkbox.checked = true;
     });
 
-    console.log('Valors dels dropdowns:', selectedValues);
+    var toggleButton = document.getElementById('toggleButton');
+    toggleButton.addEventListener('click', function () {
+        checkboxContainer.classList.toggle('hidden');
+    });
+});
 
-    const valueDropdown1rima = selectedValues.dropdown1;
-    const valueDropdown2inici = selectedValues.dropdown2;
-    const valueDropdown3silabes = selectedValues.dropdown3;
 
-    const lines = data.split('\n');
-    let matches = [];
+// Funció per gestionar el clic del botó "Mostrar/Ocultar Checkboxs"
+function toggleCheckboxes() {
+    var checkboxes = document.querySelectorAll('.clickable-checkbox');
+    var verbsList = document.getElementById("verbsList");
+    var indicatiuList = document.getElementById("indicatiuList");
+    var subjuntiuList = document.getElementById("subjuntiuList");
+    var nomsList = document.getElementById("nomsList");
+    var adjectiusList = document.getElementById("adjectiusList");
+    var determinantsList = document.getElementById("determinantsList");
 
-    for (let i = 0; i < lines.length; i++) {
-        const [firstColumn, secondColumn] = lines[i].split(',');
-        const currentTerm = secondColumn.trim().toLowerCase();
-        const firstLetter = firstColumn.trim().toLowerCase()[0]; // Obté la primera lletra
 
-        // Comprovar si el terme de cerca existeix a la segona columna
-        if (currentTerm === searchTerm) {
 
-            if (valueDropdown2inici === 'inici.indiferent') {
-                // Afegir a matches si valueDropdown2 és igual a 1
-                matches.push(firstColumn.trim());
-            } else if (valueDropdown2inici === 'inici.vocal.h' && (['a', 'e', 'i', 'o', 'u'].includes(firstLetter))) {
-                // Afegir a matches si valueDropdown2 és igual a 2 i firstLetter és una de les vocals
-                matches.push(firstColumn.trim());
-            } else if (valueDropdown2inici === 'inici.consonant' && (!['a', 'e', 'i', 'o', 'u'].includes(firstLetter))) {
-                // Afegir a matches si valueDropdown2 és igual a 3 i firstLetter NO és una de les vocals
-                matches.push(firstColumn.trim());
-            }
-            // Pots afegir més condicions aquí segons les teves necessitats
-        }
-    }
+    checkboxes.forEach(function (checkbox) {
+        checkbox.checked = true;
+    });
 
-    console.log('Matches finals:', matches);
+    verbsList.style.display = "block";
+    indicatiuList.style.display = "block";
+    subjuntiuList.style.display = "block";
+    nomsList.style.display = "block";
+    adjectiusList.style.display = "block";
+    determinantsList.style.display = "block";
 
-    // Mostrar els resultats amb salts de línia si hi ha coincidències
-    if (matches.length > 0) {
-        resultDiv.innerHTML = `${matches.join('<br>')}<br><br>`;
-    } else {
-        resultDiv.innerHTML = 'No s\'han trobat coincidències.';
-    }
 
-    console.log('Final de la funció processData');
+}
+
+
+
+function toggleVerbsList() {
+    var verbsList = document.getElementById("verbsList");
+    var checkbox1 = document.getElementById("checkbox1");
+
+    verbsList.style.display = checkbox1.checked ? "block" : "none";
+}
+
+function toggleIndicatiuList() {
+    var indicatiuList = document.getElementById("indicatiuList");
+    var indicatiucheckbox = document.getElementById("indicatiucheckbox");
+
+    indicatiuList.style.display = indicatiucheckbox.checked ? "block" : "none";
+}
+
+function toggleSubjuntiuList() {
+    var subjuntiuList = document.getElementById("subjuntiuList");
+    var subjuntiucheckbox = document.getElementById("subjuntiucheckbox");
+
+    subjuntiuList.style.display = subjuntiucheckbox.checked ? "block" : "none";
+}
+
+
+
+
+function toggleNomsList() {
+    var nomsList = document.getElementById("nomsList");
+    var checkbox2 = document.getElementById("checkbox2");
+
+    nomsList.style.display = checkbox2.checked ? "block" : "none";
+}
+
+function toggleAdjectiusList() {
+    var adjectiusList = document.getElementById("adjectiusList");
+    var checkbox3 = document.getElementById("checkbox3");
+
+    adjectiusList.style.display = checkbox3.checked ? "block" : "none";
+}
+
+
+function toggleDeterminantsList() {
+    var determinantsList = document.getElementById("determinantsList");
+    var checkbox4 = document.getElementById("checkbox4");
+
+    determinantsList.style.display = checkbox4.checked ? "block" : "none";
+}
+
+function togglePronomsList() {
+    var pronomsList = document.getElementById("pronomsList");
+    var checkbox5 = document.getElementById("checkbox5");
+
+    pronomsList.style.display = checkbox5.checked ? "block" : "none";
 }
