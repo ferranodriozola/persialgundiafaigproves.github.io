8dc124229f1180860c1427535388b335fd0d3a31
ferranodriozola <155725797+ferranodriozola@users.noreply.github.com>
2024-10-29 18:52:04 +0100

    Delete Source directory

--- Source/Pagines/3.Transports/script.js
+++
@@ -1,6 +0,0 @@
-//Back to top
-var backToTopLink = document.createElement('a');
-backToTopLink.href = '#top';
-backToTopLink.innerHTML = 'Torna a dalt';
-backToTopLink.classList.add('back-to-top-link');
-document.body.appendChild(backToTopLink);
